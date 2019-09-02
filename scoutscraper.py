d5538e67d3540dd396e85ec9822074594528a185
degleroj12 <54150903+degleroj12@users.noreply.github.com>
2019-09-02 09:46:47 -0500

    Add files via upload

---
+++ scoutscraper.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import urllib.request
+
+#Create csv
+outfile = open("scrape.csv","w",newline='')
+writer = csv.writer(outfile)
+
+foo=input('Please enter a value:')
+
+response = requests.get(foo)
+	
+
+p = soup.findAll("p",{"style" : "margin-top: 5px;"})
+
+for text in p:
+    print (text.text)
+
+	
+#Organize data into dataframe or table
+
+#Store the data
+
+#Create a table in a database based on extracted and organized data
+
+#Analyze the data
